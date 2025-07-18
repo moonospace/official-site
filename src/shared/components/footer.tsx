7e8022c35bfa1f104135374e2db6556fdd183899
Nyoman Sunima <nyomansunima@gmail.com>
2025-07-18 17:01:58 +0800

    refactor: remove components

--- src/shared/components/footer.tsx
+++ src/shared/components/footer.tsx
@@ -1,50 +1,3 @@
-import { mergeClass } from "@shared/libs"
-import { Link } from "react-router"
-
-const footerMenus = [
-  { label: "Privacy policy", href: "/privacy" },
-  { label: "Terms of use", href: "/terms" },
-]
-
-interface MenuItemProps {
-  href: string
-  children?: React.ReactNode
-  className?: string
-}
-
-function MenuItem({
-  href,
-  children,
-  className,
-}: MenuItemProps): React.ReactElement<any> {
-  return (
-    <li
-      className={`${mergeClass(
-        "flex items-center justify-center text-sm px-3 py-2 rounded-xl bg-transparent transition-all hover:-translate-x-1 hover:text-foreground/60 duration-200",
-        className,
-      )}`}
-    >
-      <Link to={href} className="w-full h-full">
-        {children}
-      </Link>
-    </li>
-  )
-}
-
-function FooterMenuList() {
-  return (
-    <div className="flex">
-      <ul className="flex items-center gap-2">
-        {footerMenus.map((item, index) => (
-          <MenuItem key={index} href={item.href}>
-            {item.label}
-          </MenuItem>
-        ))}
-      </ul>
-    </div>
-  )
-}
-
 export function Footer() {
   return (
     <footer className="flex flex-col text-sm" id="footer">
@@ -52,8 +5,9 @@ export function Footer() {
         <span className="text-sm">
           Copyright &copy; 2024 - Alright Reserved
         </span>
-
-        <FooterMenuList />
+        <span className="inline-flex gap-2">
+          Built with <i className="fi fi-sc-heart" />
+        </span>
       </div>
     </footer>
   )
