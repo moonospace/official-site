0251f4dd9782d61a06154ad40490df86d922f2b1
Nyoman Sunima <nyomansunima@gmail.com>
2025-07-21 17:46:05 +0800

    refactor: update content

--- src/shared/components/footer.tsx
+++ src/shared/components/footer.tsx
@@ -2,7 +2,7 @@ export function Footer() {
   return (
     <footer className="flex flex-col text-sm" id="footer">
       <div className="flex flex-col tablet:flex-row tablet:items-center py-7 tablet:justify-between mt-8">
-        <span className="text-sm">Design in Bali</span>
+        <span className="text-sm">Design and craft in Bali</span>
         <span className="inline-flex gap-2">
           Built with <i className="fi fi-sc-heart" />
         </span>
