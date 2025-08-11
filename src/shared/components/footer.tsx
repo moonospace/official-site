53c4a89fa65f5fafb5630eb3f95a8042750ff1fc
Nyoman Sunima <nyomansunima@gmail.com>
2025-08-12 02:19:39 +0800

    refactor: add icon and change font size

--- src/shared/components/footer.tsx
+++ src/shared/components/footer.tsx
@@ -1,8 +1,12 @@
 export function Footer() {
   return (
-    <footer className="flex flex-col text-sm" id="footer">
+    <footer className="flex flex-col" id="footer">
       <div className="flex flex-col tablet:flex-row tablet:items-center py-7 tablet:justify-between mt-8">
-        <span className="text-sm">Design and craft in Bali</span>
+        <span className="inline-flex gap-2">
+          <i className="fi fi-sc-check-circle" />
+          Design and craft in Bali
+        </span>
+
         <span className="inline-flex gap-2">
           Built with <i className="fi fi-sc-heart" />
         </span>
