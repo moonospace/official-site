b76ccb04d758d0c51563be272c2634ba9b2fee4f
Nyoman Sunima <nyomansunima@gmail.com>
2025-12-14 22:08:25 +0800

    refactor: update config for apps

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
-import { reactRouter } from "@react-router/dev/vite"
-import tailwindcss from "@tailwindcss/vite"
-import { defineConfig } from "vite"
-import tsconfigPaths from "vite-tsconfig-paths"
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import react from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-})
+  plugins: [
+    tsconfigPaths(),
+    tailwindcss(),
+    tanstackStart({
+      srcDirectory: "src/app",
+    }),
+    react(),
+    nitro(),
+  ],
+});
